09f81e20413041393460878d005c3c3da8a1b2fd
mic <micmor.m@gmail.com>
2021-07-05 13:25:41 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+const path = require("path");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  let templateVars = {};
+
+  res.render("sale", templateVars);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server listening at post 3000");
+});
